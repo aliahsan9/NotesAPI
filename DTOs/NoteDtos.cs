d934460df44d316feaea26e01e70b823fa191b98
Ali Ahsan <aliahsan8751@gmail.com>
2026-04-30 14:57:27 +0500

    added mappings

---
+++ DTOs/NoteDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace NotesAPI.DTOs
+{
+    public class NoteDtos
+    {
+            public record CreateNoteDto(string Title, string Content);
+            public record UpdateNoterDto(string Title, string Content);
+            public record NoteDto(Guid Id, string Title, string Content, DateTime CreatedAt);
+        }
+    }
---
+++ Extensions/MappingExtensions.cs
@@ -0,0 +1,12 @@
+﻿using NotesAPI.DTOs;
+using NotesAPI.Entities;
+using static NotesAPI.DTOs.NoteDtos;
+
+namespace NotesAPI.Extensions
+{
+    public static class MappingExtensions
+    {
+        public static NoteDto ToDto(this Note note)
+            => new(note.Id, note.Title, note.Content, note.CreatedAt);
+    }
+}
