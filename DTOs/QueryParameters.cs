043dbada3a8aacda215b3c43e13f1cadc9fb663e
Ali Ahsan <aliahsan8751@gmail.com>
2026-04-30 19:56:23 +0500

    added pagination, filtering and searching

---
+++ DTOs/QueryParameters.cs
@@ -0,0 +1,20 @@
+﻿namespace NotesAPI.DTOs
+{
+    public class QueryParameters
+    {
+        private const int MaximumPageSize = 50;
+
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value > MaximumPageSize ? MaximumPageSize : value;
+
+        }
+        public string SearchTerm { get; set; } = string.Empty;
+        public string SortBy { get; set; } = "CreatedAt";
+        public bool IsDecending { get; set; } = true;
+    }
+}
--- Interafces/INoteService.cs
+++ Interafces/INoteService.cs
@@ -1,10 +1,11 @@
-﻿using static NotesAPI.DTOs.NoteDtos;
+﻿using NotesAPI.DTOs;
+using static NotesAPI.DTOs.NoteDtos;
 
 namespace NotesAPI.Interafces
 {
     public interface INoteService
     {
-        Task<IEnumerable<NoteDto>> GetAllAsync();
+        Task<IEnumerable<NoteDto>> GetAllAsync(QueryParameters query);
         Task<NoteDto> GetByIdAsync(Guid Id);
         Task<NoteDto> CreateAsync(CreateNoteDto dto);
         Task<bool> UpdateAsync(Guid Id, UpdateNoteDto dto);
